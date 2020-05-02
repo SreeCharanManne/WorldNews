753ec13cdea685e97fbd528e4b226a1e773f7c57
CharanManne <sreecharanmanne2000@gmail.com>
2020-05-02 23:48:51 +0530

    first commit

---
+++ worldnews/src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FooterComponent = () => {
+    return (
+        <div className="footer">
+            <footer className="page-footer blue-grey darken-2200">
+                <div className="container  footer-copyright">
+                    All rights owned by CharanManne
+                    <a href="https://github.com/SreeCharanManne/NewsApp">Github</a>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default FooterComponent;
